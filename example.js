92d79f9b750e4f9b9d2725922dbe97788ccef8e9
Nick McCurdy <thenickperson@gmail.com>
2016-11-22 15:20:28 -0500

    Refactor the hello example into an object with methods

--- example.js
+++ example.js
@@ -1,30 +1,33 @@
 /* global test, given, rejects, resolves, returns, throws */
 'use strict'
 require('.').setup()
+
+var hello = {
+  // Synchronous
+  sync (name) {
+    if (name) return `Hello, ${name}!`
+    else throw new Error('Missing name.')
+  },
+
+  // Asynchronous with Promises
+  promise (name) {
+    return new Promise(resolve => setTimeout(resolve, 1000))
+      .then(() => hello.sync(name))
+  }
+}
+
 var suite
 
 // Synchronous suite
-
-function hello (name) {
-  if (name) return `Hello, ${name}!`
-  else throw new Error('Missing name.')
-}
-
-suite = test('hello',
-             hello,
+suite = test('hello.sync',
+             hello.sync,
              given(['Nick'], returns('Hello, Nick!')),
              throws('Missing name'))
 suite()
 
 // Asynchronous suite with Promises
-
-function helloPromise (name) {
-  return new Promise(resolve => setTimeout(resolve, 1000))
-    .then(() => hello(name))
-}
-
-suite = test('helloPromise',
-             helloPromise,
+suite = test('hello.promise',
+             hello.promise,
              given(['Nick'], resolves('Hello, Nick!')),
              rejects('Missing name'))
 suite()
