a257c2c2e2dddf2330570adfc39b3e3f1833619d
Nick McCurdy <thenickperson@gmail.com>
2016-11-26 12:45:40 -0500

    Minor refactorings in index.js

--- index.js
+++ index.js
@@ -3,31 +3,26 @@ var fs = require('fs')
 var path = require('path')
 var Test = require('./Test')
 
-var MATCHERS_DIR = path.join(__dirname, 'matchers')
-
 module.exports = {
-  matchers: fs.readdirSync(MATCHERS_DIR).reduce((memo, file) => {
-    return Object.assign({}, memo, {
-      [path.basename(file, '.js')]: require(`${MATCHERS_DIR}/${file}`)
-    })
-  }, {}),
+  matchers: fs.readdirSync(path.join(__dirname, 'matchers'))
+              .map(file => path.basename(file, '.js'))
+              .reduce((memo, matcher) => {
+                memo[matcher] = require(`./matchers/${matcher}`)
+                return memo
+              }, {}),
 
   setup (target) {
-    var matchers = Object.keys(this.matchers).reduce((memo, key) => {
-      var MatcherClass = this.matchers[key]
-      return Object.assign({}, memo, {
-        [key.toLowerCase()]: (arg1, arg2) => new MatcherClass(arg1, arg2)
-      })
+    var matchers = Object.keys(this.matchers).reduce((memo, matcher) => {
+      var Class = this.matchers[matcher]
+      memo[matcher.toLowerCase()] = (arg1, arg2) => new Class(arg1, arg2)
+      return memo
     }, {})
-    Object.assign(target || global,
-                  matchers,
-                  { test: this.test })
+    Object.assign(target || global, matchers, { test: this.test })
   },
 
   Test,
 
   test (name, subject) {
-    var runnables = Array.from(arguments).slice(2)
-    return new Test(name, subject, runnables)
+    return new Test(name, subject, Array.from(arguments).slice(2))
   }
 }
