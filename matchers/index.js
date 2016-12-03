ce56fe5ce8c1da7c6e231691ae39753c7fb96f63
Nick McCurdy <thenickperson@gmail.com>
2016-12-03 09:23:56 -0500

    Use webpack to bundle source files

--- matchers/index.js
+++ matchers/index.js
@@ -1,11 +1,11 @@
 'use strict'
-var fs = require('fs')
 var path = require('path')
 
-module.exports = fs.readdirSync(__dirname)
-                   .map(file => path.basename(file, '.js'))
-                   .filter(file => file !== 'index')
-                   .reduce((memo, matcher) => {
-                     memo[matcher] = require(`./${matcher}`)
-                     return memo
-                   }, {})
+function requireNamespace (requireContext) {
+  return requireContext.keys().reduce((memo, key) => {
+    memo[path.basename(key, '.js')] = requireContext(key)
+    return memo
+  }, {})
+}
+
+module.exports = requireNamespace(require.context('.', false, /\/(?!index)/))
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: './example',
+  output: {
+    filename: 'bundle.js'
+  },
+  target: 'node'
+}
