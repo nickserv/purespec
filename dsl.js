7f72cefb774de0c8146b0baec0baee0105681883
Nick McCurdy <thenickperson@gmail.com>
2016-12-02 22:10:31 -0500

    Extract function for matcher DSL

--- dsl.js
+++ dsl.js
@@ -3,13 +3,16 @@ var matchers = require('./matchers')
 var Test = require('./Test')
 
 module.exports = {
-  setup (target) {
-    var matcherDSL = Object.keys(matchers).reduce((memo, matcher) => {
+  matchers () {
+    return Object.keys(matchers).reduce((memo, matcher) => {
       var Class = matchers[matcher]
       memo[matcher.toLowerCase()] = (arg1, arg2) => new Class(arg1, arg2)
       return memo
     }, {})
-    Object.assign(target || global, matcherDSL, { test: this.test })
+  },
+
+  setup (target) {
+    Object.assign(target || global, this.matchers(), { test: this.test })
   },
 
   test (name, subject) {
