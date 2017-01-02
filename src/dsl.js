d1756e90b477dda90dff035c9cb2e24130bb46d1
Nick McCurdy <thenickperson@gmail.com>
2017-01-02 06:39:52 -0500

    Support falsy targets

--- src/dsl.js
+++ src/dsl.js
@@ -3,7 +3,9 @@ var matchers = require('./matchers')
 var Test = require('./Test')
 
 function dsl (target) {
-  Object.assign(target || global, dsl.matchers(), { test: dsl.test })
+  Object.assign(target === undefined ? global : target,
+                dsl.matchers(),
+                { test: dsl.test })
 }
 
 dsl.matchers = () => {
