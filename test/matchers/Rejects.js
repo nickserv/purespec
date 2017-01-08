0a0ac5bb5b2f3f6e5cdaf4bd64abd97d8cfd9e65
Nick McCurdy <thenickperson@gmail.com>
2017-01-08 02:26:56 -0500

    Stub more test cases for run()

--- test/matchers/Rejects.js
+++ test/matchers/Rejects.js
@@ -8,8 +8,18 @@ describe('Rejects matcher', () => {
   })
 
   describe('.prototype.run()', () => {
-    it('runs its subject as a Promise, asserting a rejection with the given reason', () => {
-      return rejects.run(example.hello.promise)
+    context('given a subject that rejects with a String', () => {
+      it('runs its subject as a Promise, asserting a rejection with the given reason')
+    })
+
+    context('given a subject that rejects with an Error', () => {
+      it('runs its subject as a Promise, asserting a rejection with the given reason', () => {
+        return rejects.run(example.hello.promise)
+      })
+    })
+
+    context('given a subject that resolves', () => {
+      it('runs its subject as a Promise, failing to assert a rejection with the given reason')
     })
   })
 
