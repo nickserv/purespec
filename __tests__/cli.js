01fab95552705421f0ec9f2a98f08a877abb8d4b
Nick McCurdy <thenickperson@gmail.com>
2017-02-09 04:33:56 -0500

    Expect an error when the CLI is given an invalid module

--- __tests__/cli.js
+++ __tests__/cli.js
@@ -1,17 +1,34 @@
+console.log = jest.fn()
+
 describe('CLI', function () {
   beforeEach(function () {
-    this.example = require('../src/example')
-    this.example.run = jest.fn(this.example.run)
-
-    process.argv[2] = 'src/example'
-    console.log = jest.fn()
-    this.cli = require('../src/cli')
+    jest.resetModules()
+    this.cli = '../src/cli'
   })
 
-  it('runs the given file and prints its results', function () {
-    return this.cli.then(() => {
-      expect(this.example.run).toHaveBeenCalled()
-      expect(console.log).toHaveBeenCalled()
+  describe('given a valid module', function () {
+    beforeEach(function () {
+      this.example = require('../src/example')
+      this.example.run = jest.fn(this.example.run)
+
+      process.argv[2] = 'src/example'
+    })
+
+    it('runs the given module and prints its results', function () {
+      return require(this.cli).then(() => {
+        expect(this.example.run).toHaveBeenCalled()
+        expect(console.log).toHaveBeenCalled()
+      })
+    })
+  })
+
+  describe('given an invalid module', function () {
+    beforeEach(function () {
+      process.argv[2] = ''
+    })
+
+    it('throws an error', function () {
+      expect(() => require(this.cli)).toThrow()
     })
   })
 })
