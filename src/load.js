c23f9754dc931f0bc0ffae1c7f8a4945d274676a
Nick McCurdy <thenickperson@gmail.com>
2017-02-12 04:06:55 -0500

    Fix missing globals in PureSpec modules

--- src/load.js
+++ src/load.js
@@ -1,10 +1,9 @@
-const _ = require('lodash/fp')
 const fs = require('fs')
 const dsl = require('./dsl')
 const vm = require('vm')
 
 module.exports = function (file) {
   return vm.runInNewContext(fs.readFileSync(file),
-                            _.assign(dsl, { module }),
+                            Object.assign({ module, require }, global, dsl),
                             { filename: file })
 }
