b7a43740bfa81aa41ddc575d332d736b84e23a45
Nick McCurdy <nick@nickmccurdy.com>
2017-03-17 20:10:28 -0400

    Only use lodash/fp

--- src/ComparisonResult.js
+++ src/ComparisonResult.js
@@ -1,9 +1,9 @@
-const _ = require('lodash/core')
+const _ = require('lodash/fp')
 const Result = require('./Result')
 
 module.exports = class ComparisonResult extends Result {
   constructor (runnable, actual, expected) {
-    super(runnable, !_.isEqual(actual, expected) || undefined)
+    super(runnable, !_.isEqual(actual)(expected) || undefined)
     this.actual = actual
     this.expected = expected
   }
