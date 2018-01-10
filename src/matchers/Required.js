f8a66a7007c3a952505c9741b54b7134845279bd
Nick McCurdy <nick@nickmccurdy.com>
2018-01-10 13:09:35 -0500

    Fix using Required from a VM in a different directory

--- src/matchers/Required.js
+++ src/matchers/Required.js
@@ -2,7 +2,8 @@ const Test = require('./Test')
 
 module.exports = class Required extends Test {
   constructor (name, ...runnables) {
-    super(require(name), ...runnables)
+    const subject = require(require.resolve(name, { paths: [process.cwd()] }))
+    super(subject, ...runnables)
     this.name = name
   }
 
