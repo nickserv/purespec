dd1a0b6663fd5438e99d33c20f70558951d77ba4
Nick McCurdy <nick@nickmccurdy.com>
2018-01-11 06:47:28 -0500

    Add pending pure specs

--- pure_test.js
+++ pure_test.js
@@ -2,11 +2,29 @@
 
 module.exports = required(
   '.',
+  prop('ComparisonResult'),
+  prop('NestedResult'),
+  prop('Result'),
+  prop('cli'),
+  prop('dsl'),
   prop(
     'indent',
     given('', returns('  ')),
     given('one', returns('  one')),
     given('one\ntwo', returns('  one\n  two')),
     given('  one', returns('    one'))
+  ),
+  prop('load'),
+  prop(
+    'matchers',
+    prop('Given'),
+    prop('Nested'),
+    prop('Prop'),
+    prop('Rejects'),
+    prop('Required'),
+    prop('Resolves'),
+    prop('Returns'),
+    prop('Test'),
+    prop('Throws')
   )
 )
