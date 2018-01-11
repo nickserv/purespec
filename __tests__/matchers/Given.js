1c04bbfac193bb653adbf4f420330dd61463b80c
Nick McCurdy <nick@nickmccurdy.com>
2018-01-11 10:06:19 -0500

    Implement Given as a Nested matcher

--- __tests__/matchers/Given.js
+++ __tests__/matchers/Given.js
@@ -8,7 +8,7 @@ describe('Given matcher', () => {
     it('returns a new Given with the given args and matcher', () => {
       expect(given).toMatchObject({
         args: ['World'],
-        matcher: new purespec.matchers.Returns('Hello, World!')
+        runnables: [new purespec.matchers.Returns('Hello, World!')]
       })
     })
   })
@@ -24,12 +24,6 @@ describe('Given matcher', () => {
   describe('.prototype.toString()', () => {
     it('returns a String representation with its args', () => {
       expect(given.toString()).toBe('given World')
-    })
-  })
-
-  describe('.prototype.toTree()', () => {
-    it('returns a String representation with its args and matcher', () => {
-      expect(given.toTree()).toBe('given World\n  returns Hello, World!')
     })
   })
 })
