9efdf0953b976e0916401ea14600ee29fa58efc0
Nick McCurdy <nick@nickmccurdy.com>
2018-01-25 13:39:15 -0500

    Revert "Configure eslint globals dynamically"
    This reverts commit c9925fdd50e6875888edcbb1a09d54fc62b7e81f.

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-const purespec = require('.')
-
-module.exports = {
-  extends: "./package.json",
-  globals: Object.keys(purespec.dsl).reduce((memo, name) =>
-    Object.assign({ [name]: false}, memo)
-  )
-}
